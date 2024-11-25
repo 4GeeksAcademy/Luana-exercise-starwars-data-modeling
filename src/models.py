3eb4f34485372884d4ffeb2888214be64b914640
Luana Tajes <lmluana27@gmail.com>
2024-11-25 18:38:31 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -13,20 +13,38 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    eye_Colour = Column(String(250), nullable=False)
+    hair_colour = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    climate =  Column(String(250), nullable=False)
+
+class Favourite(Base):
+    __tablename__ = 'favourite'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey("person.id"))
+    person = relationship(Person)
+    character_id = Column(Integer, ForeignKey("character.id"))
+    character = relationship(Character)
+    planets_id = Column(Integer, ForeignKey("planets.id"))
+    plantes = relationship(Planets)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
